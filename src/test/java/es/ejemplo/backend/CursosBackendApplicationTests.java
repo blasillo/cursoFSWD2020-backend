8b1746ee8cef8f3dc5a7be6c4cb4316995587f9e
blas <btorregrosa@infor.uva.es>
2022-09-04 21:59:45 +0200

    Inicio

---
+++ src/test/java/es/ejemplo/backend/CursosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ejemplo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
